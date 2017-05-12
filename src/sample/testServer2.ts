bd04d06dc85276e27bc18a5f898f10de3a2d348a
qmShen <joyshen06@gmail.com>
2017-05-12 11:35:35 +0800

    version1

--- src/sample/testServer2.ts
+++ src/sample/testServer2.ts
@@ -5,12 +5,21 @@ import * as koa from "koa"
 const app = new koa()
 
 const server = http.createServer(app.callback())
-app.use(monitor(server, {path: '/status', statusHtmlPage: 'index.html'}))
+app.use(monitor(server, { path: '/status', statusHtmlPage: 'index.html' }))
 
-app.use(function *() {
-  if (this.path === '/') {
-    this.body = 'Hello World'
-  }
+app.use(function* (next) {
+    console.log('middleware')
+    yield next;
+});
+
+
+app.use(function* () {
+    if (this.path === '/') {
+        console.log('test')
+        this.body = 'Hello World'
+    }
 })
 
+
+app.listen(3001)
 server.listen(3000)
---
+++ src/sample/test_server.ts
@@ -0,0 +1,18 @@
+
+import * as http from 'http';
+import * as monitor from '../index.js'
+import * as koa from "koa"
+import * as Router from 'koa-router'
+const app = new koa()
+const server = http.createServer(app.callback())
+app.use(monitor(server, {path: '/status', statusHtmlPage: 'index.html'}))
+
+let router = new Router()
+    .get('/',  async (ctx, next) => {
+        console.log('root./')
+        ctx.body = 'Hello World3'
+    })
+    
+app.use(router.routes())
+app.listen(3001)
+server.listen(3000)
